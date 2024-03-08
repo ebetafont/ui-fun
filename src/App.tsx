4be5ae3ccf641f5880e7fde9994fa18107acd4f6
ebetafont <ebetafont@gmail.com>
2024-03-07 20:52:56 -0500

    Change input number icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import { BiChevronUp, BiChevronDown } from "react-icons/bi"
-import { BiSolidHelpCircle } from "react-icons/bi"
+import { BiChevronUp, BiChevronDown, BiSolidHelpCircle, BiSolidDownArrow, BiSolidUpArrow } from "react-icons/bi"
+
 
 
 function App() {
@@ -28,12 +28,12 @@ function App() {
         <div className='flex gap-10 text-stone-400 hover:text-stone-50 pl-2 m-5'> 
           <div className="primer">
             <label htmlFor="linear">Linear:</label>
-            <input type="checkbox" name='linear' checked className='p-10' />
+            <input type="checkbox" name='linear' className='p-10' />
           </div>
           <div className="quantity">
             <input type="number" value='3' id='input-linear' className='bg-neutral-700 hover:bg-neutral-600 rounded-md h-8 w-20' />
-            <button className="quantity-button quantity-up" onClick={e=>steppingUp("input-linear")}><BiChevronUp /></button>
-            <button className="quantity-button quantity-down" onClick={e=>steppingDown("input-linear")}><BiChevronDown /></button>
+            <button className="quantity-button quantity-up" onClick={e=>steppingUp("input-linear")}><BiSolidUpArrow className='text-xs ml-1 text-center' /></button>
+            <button className="quantity-button quantity-down" onClick={e=>steppingDown("input-linear")}><BiSolidDownArrow className='text-xs ml-1 text-center' /></button>
           </div>
         </div>
 
@@ -47,15 +47,15 @@ function App() {
           <div className="quantity">
             <label htmlFor="min">Min:</label>
             <input type="number" name='min' value='0' id='input-min' className='bg-neutral-700 hover:bg-neutral-600 rounded-md h-8 w-20' />
-            <button className="quantity-button quantity-up" onClick={e=>steppingUp("input-min")}><BiChevronUp /></button>
-            <button className="quantity-button quantity-down" onClick={e=>steppingDown("input-min")}><BiChevronDown /></button>
+            <button className="quantity-button quantity-up" onClick={e=>steppingUp("input-min")}><BiSolidUpArrow className='text-xs ml-1 text-center' /></button>
+            <button className="quantity-button quantity-down" onClick={e=>steppingDown("input-min")}><BiSolidDownArrow className='text-xs ml-1 text-center' /></button>
           </div>
 
           <div className="quantity">
             <label htmlFor="max">Max:</label>
             <input type="number" name='max' value='2' id='input-max' className='bg-neutral-700 hover:bg-neutral-600 rounded-md h-8 w-20' />
-            <button className="quantity-button quantity-up" onClick={e=>steppingUp("input-max")}><BiChevronUp /></button>
-            <button className="quantity-button quantity-down" onClick={e=>steppingDown("input-max")}><BiChevronDown /></button>
+            <button className="quantity-button quantity-up" onClick={e=>steppingUp("input-max")}><BiSolidUpArrow className='text-xs ml-1 text-center' /></button>
+            <button className="quantity-button quantity-down" onClick={e=>steppingDown("input-max")}><BiSolidDownArrow className='text-xs ml-1 text-center' /></button>
           </div>
 
         </div>
